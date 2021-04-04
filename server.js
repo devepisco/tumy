5d182dea58e536b9fa6f7d7bb710f649f12512ae
Rodrigo Berrios <52769216+RodrigoBS98@users.noreply.github.com>
2021-04-04 13:24:53 -0500

    feat: add server config to api

---
+++ server.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+
+// Setup express server port from ENV, default: 3000
+app.set("port", process.env.PORT || 3000);
+
+// Enable only in development HTTP request logger middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+// for parsing json
+app.use(
+  bodyParser.json({
+    limit: "20mb",
+  })
+);
+// for parsing application/x-www-form-urlencoded
+app.use(
+  bodyParser.urlencoded({
+    limit: "20mb",
+    extended: true,
+  })
+);
+
+// Init all other stuff
+app.use(cors());
+app.use(passport.initialize());
+app.use(express.static("public"));
+app.use(helmet());
+
+app.listen(app.get("port"), () => {
+  if (process.env.NODE_ENV === "development") {
+    // Prints initialization
+    console.log("****************************");
+    console.log("*    Starting Server");
+    console.log(`*    Port: ${process.env.PORT || 3000}`);
+    console.log(`*    NODE_ENV: ${process.env.NODE_ENV}`);
+    console.log(`*    Database: MongoDB`);
+    console.log("****************************");
+  }
+  if (process.env.NODE_ENV === "production") {
+    // Prints initialization
+    console.log(`Server listening on port ${process.env.PORT}`);
+  }
+});
+
+module.exports = { app };
