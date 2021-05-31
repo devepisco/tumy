b288174b2e6481f349b564b223941b4070243595
Rodrigo Berrios <ti09@emilima.com.pe>
2021-05-31 09:52:03 -0500

    fix: updated user

--- app/controllers/users/updateMe.js
+++ app/controllers/users/updateMe.js
@@ -1,22 +1,14 @@
 const { User } = require("../../models/User");
 const { structure, objSuccess } = require('../../middlewares/utils');
 const { getUserIdFromToken  } = require("../auth/helpers/getUserIdFromToken");
+const { matchedData } = require("express-validator");
 
 const updateMe = structure(async (req, res) =>{
-    const userData = {
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        IDType:req.body.typeID,
-        IDNumber:req.body.numID,
-        business:req.body.business,
-        phone:req.body.phone,
-        email:req.body.email
-    }
-    const userId = getUserIdFromToken(req.headers.authorization.replace("Bearer ", "").trim())
+
+    const userData = matchedData(req)
+    const userId = req.user._id
     const updatedUser = await User.findByIdAndUpdate(userId, userData, {new:true});
-
     // const updatedUser = await findUserById(userId)
-
     res.status(200).json(
         objSuccess(updatedUser,"Se actualizaron los datos del usuario")
     );
