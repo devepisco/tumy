d74845d2f9da5ad8e722897f0ea4cc34a2b164c7
Rodrigo Berrios <rodrigoberrios1998@gmail.com>
2021-06-07 19:13:39 -0500

    feat: preference mercadopago

--- app/controllers/paymentCheckout/helpers/createNewPreference.js
+++ app/controllers/paymentCheckout/helpers/createNewPreference.js
@@ -4,6 +4,11 @@ const createNewPreference = async (item, payer) => {
   let preference = {
     items: [item],
     payer,
+    expires: true,
+    payment_methods: {
+      excluded_payment_methods: [{ id: "ticket" }, { id: "atm" }],
+      default_installments: 1,
+    },
   };
   if (payer?.identification?.type && payer.identification.type === "CE") {
     payer.identification.type = "C.E";
