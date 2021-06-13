d8952deb971c4d45a0f57c6533d99d16888c75cb
Rodrigo Berrios <rodrigoberrios1998@gmail.com>
2021-06-13 15:32:12 -0500

    feat: save pyment in route

--- app/controllers/users/index.js
+++ app/controllers/users/index.js
@@ -1,19 +1,21 @@
-const { updateMe } = require('./updateMe')
-const { createService } = require('./createService');
-const { saveDetailsService } = require('./saveDetailsService');
-const { getPaymentMethods } = require('./getPaymentMethods');
-const { getGlobalDataServices } = require('./getGlobalDataServices');
-const { getDetailDataFromService } = require('./getDetailDataFromService');
-const { getDriverFromService } = require('./getDriverFromService');
-const { cancelService } = require('./cancelService');
+const { updateMe } = require("./updateMe");
+const { createService } = require("./createService");
+const { saveDetailsService } = require("./saveDetailsService");
+const { getPaymentMethods } = require("./getPaymentMethods");
+const { getGlobalDataServices } = require("./getGlobalDataServices");
+const { getDetailDataFromService } = require("./getDetailDataFromService");
+const { getDriverFromService } = require("./getDriverFromService");
+const { cancelService } = require("./cancelService");
+const { savePaymentService } = require("./savePaymentService");
 
 module.exports = {
-    updateMe,
-    createService,
-    saveDetailsService,
-    getPaymentMethods,
-    getGlobalDataServices,
-    getDetailDataFromService,
-    getDriverFromService,
-    cancelService
-}
+  updateMe,
+  createService,
+  saveDetailsService,
+  getPaymentMethods,
+  getGlobalDataServices,
+  getDetailDataFromService,
+  getDriverFromService,
+  cancelService,
+  savePaymentService,
+};
