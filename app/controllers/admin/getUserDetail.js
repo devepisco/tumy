7b67a67a4f17949a8af72702da03d162a33670b0
meeighteen <josuee.ald@gmail.com>
2021-06-14 15:52:18 -0500

    fix: getUserDetail

--- app/controllers/admin/getUserDetail.js
+++ app/controllers/admin/getUserDetail.js
@@ -5,7 +5,8 @@ const { isAdmin } = require("../admin/helpers");
 const getUserDetail = structure(async (req, res) => {
   const isUserAdmin = await isAdmin(req.user._id);
   if (!isUserAdmin) return handleError(res, 400, "Usuario no permitido.");
-  const user = findUserById(req.params.id);
+  const user = await findUserById(req.params.id);
+  if(!user) return handleError(res,400, "Usuario no encontrado.")
   res.status(200).json(objSuccess(user));
 });
 
