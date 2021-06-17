e2c13d5d97888a50f36838c4e833432ff3794b52
Rodrigo Berrios <rodrigoberrios1998@gmail.com>
2021-06-17 01:26:51 -0500

    feat: handle errors of culqi

--- app/controllers/users/savePaymentService.js
+++ app/controllers/users/savePaymentService.js
@@ -3,12 +3,14 @@ const { structure } = require("../../middlewares/utils");
 const { updateItem, getItem } = require("../../middlewares/db");
 const { createToken, createCharge } = require("../culqi/helpers");
 const { RequestService } = require("../../models/NewServices");
+const Exceptions = require("../../../errors/Exceptions");
 
 const savePaymentService = structure(async (req, res) => {
   const { info } = req.body;
   const data = JSON.parse(info);
   try {
     const token = await createToken(data);
+    console.log(token);
     const service = await getItem(data._id, RequestService);
     const charge = await createCharge({
       amount: `${service.costo}`.replace(".", ""),
@@ -21,8 +23,13 @@ const savePaymentService = structure(async (req, res) => {
         phone_number: req.user.phone,
       },
     });
+    console.log(charge);
   } catch (err) {
-    console.log(err)
+    throw new Exceptions(
+      500,
+      err.user_message ||
+        "Ocurrió un error interno, porfavor vuelva a intentarlo mas tarde"
+    );
   }
   const updatedService = await updateItem(
     data._id,
