ecf79dc8f4e8cc5e297fe938fdc80192d86030ea
meeighteen <josuee.ald@gmail.com>
2021-06-26 23:49:50 -0500

    fix: setComissions

--- app/controllers/requestService/setComissions.js
+++ app/controllers/requestService/setComissions.js
@@ -6,9 +6,10 @@ const setComissions = async(req, res) => {
     const globalState = await findGlobalState('entregado');
     const services = await RequestService.find({globalState:globalState._id});
     for(i in services){
+        const amount = Comission.amount* services[i].costo;
         services[i].detail.comission= { 
             _id : Comission._id,
-            amount : Comission.amount* services[i].costo
+            amount : `${amount.toFixed(2)}`
         }
     }
     res.status(200).json(objSuccess(services,"Registros actualizados correctamente"));
