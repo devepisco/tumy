6aedf749b80fa80fea70460cdbd4c0a5280af0ce
meeighteen <josuee.ald@gmail.com>
2021-07-12 21:11:45 -0500

    update: accept/reject driver

--- app/controllers/auth/registerDriver.js
+++ app/controllers/auth/registerDriver.js
@@ -1,33 +1,54 @@
-const Exceptions = require('../../../errors/Exceptions');
-const RequestDriver = require('../../models/DriverRequests');
-const { structure, objSuccess } = require('../../middlewares/utils');
+const Exceptions = require("../../../errors/Exceptions");
+const RequestDriver = require("../../models/DriverRequests");
+const { User } = require("../../models/User");
+const { structure, objSuccess } = require("../../middlewares/utils");
 
-const registerDriver = structure(async (req , res)=> {
-    const findedUser = await RequestDriver.findOne({email: req.body.email})
-    if(findedUser) throw new Exceptions(400, "El correo ya fue registrado")
+const registerDriver = structure(async (req, res) => {
+  const findedEmail = await RequestDriver.findOne({ email: req.body.email });
+  if (findedEmail) throw new Exceptions(400, "El correo ya fue registrado");
 
-    const findedUser2 = await RequestDriver.findOne({IDNumber: req.body.numID})
-    if(findedUser2) throw new Exceptions(400, "El DNI ya fue registrado")
+  const findedIDNumber = await RequestDriver.findOne({ IDNumber: req.body.numID });
+  if (findedIDNumber) throw new Exceptions(400, "El DNI ya fue registrado");
 
-    const registeredDriver = new RequestDriver({
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
-        IDType:req.body.typeID,
-        IDNumber:req.body.numID,
-        SOATNumber:req.body.numSOAT,
-        VehicleRegistration:req.body.numPlaca,
-        propertyCardNumber:req.body.numTarjetaPropiedad,
-        phone:req.body.phone,
-        email:req.body.email,
-        password:req.body.password,
-        profilePicture:req.file.filename,
-    })
-    await registeredDriver.save()
+  const registeredDriver = new RequestDriver({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    IDType: req.body.typeID,
+    IDNumber: req.body.numID,
+    SOATNumber: req.body.numSOAT,
+    VehicleRegistration: req.body.numPlaca,
+    propertyCardNumber: req.body.numTarjetaPropiedad,
+    phone: req.body.phone,
+    email: req.body.email,
+    password: req.body.password,
+    profilePicture: req.file.filename,
+  });
+  await registeredDriver.save();
 
-    return res.status(200).json(objSuccess(
-        data ={},
-        message = "Usuario motorizado registrado correctamente"
-    ))
+  const USER = new User({
+    firstname: req.body.firstname,
+    lastname: req.body.lastname,
+    IDType: req.body.typeID,
+    IDNumber: req.body.numID,
+    SOATNumber: req.body.numSOAT,
+    VehicleRegistration: req.body.numPlaca,
+    propertyCardNumber: req.body.numTarjetaPropiedad,
+    phone: req.body.phone,
+    email: req.body.email,
+    password: req.body.password,
+    profilePicture: req.file.filename,
+    isBlocked: true,
+  });
+  await USER.save();
+
+  return res
+    .status(200)
+    .json(
+      objSuccess(
+        (data = {}),
+        (message = "Usuario motorizado registrado correctamente")
+      )
+    );
 });
 
-module.exports = { registerDriver }
+module.exports = { registerDriver };
--- app/controllers/drivers/acceptRequestDriver.js
+++ app/controllers/drivers/acceptRequestDriver.js
@@ -1,46 +1,37 @@
 const { matchedData } = require("express-validator");
 const { structure, handleError, objSuccess } = require("../../middlewares/utils");
+const { findById } = require("../../models/DriverRequests");
+// const { decrypt } = require("../../middlewares/crypto");
 const RequestDriver = require("../../models/DriverRequests");
 const { User } = require("../../models/User");
 
 const acceptRequestDriver = structure( async (req, res) => {
     const { id, action, reason } = matchedData(req);
+    let foundUser;
     let message = "";
-    let newUser = {};
     const FoundRequestDriver = await RequestDriver.findById(id);
     if(!FoundRequestDriver) return handleError(res,400, "No se encontró la solicitud del motorizado.")
     switch(action){
         case 'accept':
-            const foundEmailUser = await User.findOne({email: FoundRequestDriver.email})
-            const foundIDUser = await User.findOne({IDNumber: FoundRequestDriver.IDNumber})
-            if(foundEmailUser || foundIDUser) return handleError(res, 400, "El Motorizado ya fue aceptado")
-        
-            FoundRequestDriver.status = "Aceptada";
+            foundUser = await User.findOne({email: FoundRequestDriver.email})
+            if(FoundRequestDriver.status == 'Aceptado') return handleError(res, 400, "El Motorizado ya fue aceptado")
+
+            /* Activar Usuario Motorizado */
+            foundUser.isBlocked = false;
+
+            FoundRequestDriver.status = "Aceptado";
             FoundRequestDriver.reason = reason;
-        
-            await User.create({
-                role:FoundRequestDriver.role,
-                firstname: FoundRequestDriver.firstname,
-                lastname:FoundRequestDriver.lastname,
-                IDType:FoundRequestDriver.IDType,
-                IDNumber:FoundRequestDriver.IDNumber,
-                SOATNumber:FoundRequestDriver.SOATNumber,
-                VehicleRegistration: FoundRequestDriver.VehicleRegistration,
-                propertyCardNumber: FoundRequestDriver.propertyCardNumber,
-                business:FoundRequestDriver.business,
-                phone:FoundRequestDriver.phone,
-                email:FoundRequestDriver.email,
-                password:`${FoundRequestDriver.password}`,
-                profilePicture:FoundRequestDriver.profilePicture
-            });
+
+            
             message = "La solicitud de motorizado fue aceptada correctamente";
             break;
         case 'reject':
-            if(FoundRequestDriver.status == "Rechazada") {
+            if(FoundRequestDriver.status == "Rechazado") {
                 message = "La solicitud del motorizado ya fue rechazada."
                 break;
             }
-            FoundRequestDriver.status = "Rechazada";
+            await User.remove({email: FoundRequestDriver.email});
+            FoundRequestDriver.status = "Rechazado";
             FoundRequestDriver.reason = reason;
             message = "La solicitud de motorizado fue rechazada correctamente";
             break
