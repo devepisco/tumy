2a686cff19cd1e2c6fbda801bcb06225677a6dbd
meeighteen <josuee.ald@gmail.com>
2021-07-13 00:05:53 -0500

    fix: getDetailDataFromService

--- app/controllers/users/getDetailDataFromService.js
+++ app/controllers/users/getDetailDataFromService.js
@@ -6,7 +6,10 @@ const getDetailDataFromService = structure( async (req, res) => {
     const IdService = isIDGood(req.params.id);
     const FoundService = await RequestService.findById(IdService)
     if(!FoundService) return handleError(res, 404, "No se encontró la solicitud de servicio");
-    const data = await RequestService.findOne({ _id : IdService},{__v:0}).populate('detailState._id',{_id:0,IdName:0,__v:0}).populate('globalState',{_id:0,IdName:0,__v:0});
+    const data = await RequestService.findOne({ _id : IdService},{__v:0})
+        .populate('detailState._id',{_id:0,IdName:0,__v:0})
+        .populate('globalState',{_id:0,IdName:0,__v:0})
+        .populate('detail.driverUser',{firstname:1, lastname: 1});
     res.status(200).json(objSuccess(data));
 });
 
