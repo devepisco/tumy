fe1b03e9602a3302af73f14ff7b41de8f90f1cb9
meeighteen <josuee.ald@gmail.com>
2021-07-14 20:43:19 -0500

    fix: getRequestDrivers

--- app/controllers/drivers/getRequestDriver.js
+++ app/controllers/drivers/getRequestDriver.js
@@ -1,16 +1,20 @@
 const { matchedData } = require("express-validator");
 const { getItemsWithPagination } = require("../../middlewares/db");
-const { structure } = require("../../middlewares/utils");
+const { structure, objSuccess } = require("../../middlewares/utils");
 const RequestDriver = require("../../models/DriverRequests");
 
 const getRequestDriver = structure(async (req, res) => {
   const { status } = matchedData(req);
-  const drivers = await getItemsWithPagination(
-    req.query,
-    { status: status },
-    RequestDriver
-  );
-  return res.status(200).json(drivers);
+  // const drivers = await getItemsWithPagination(
+  //   req.query,
+  //   { status: status},
+  //   RequestDriver
+  // )
+  let drivers;
+  if (status) drivers = await RequestDriver.find({ status }, { password: 0 });
+  else drivers = await RequestDriver.find({}, { password: 0 });
+
+  return res.status(200).json(objSuccess(drivers));
 });
 
 module.exports = { getRequestDriver };
