c0864f99f7c4d34439baff2538c20ccec751daf8
meeighteen <josuee.ald@gmail.com>
2021-09-17 14:48:23 -0500

    fixes alidateToken

--- app/controllers/auth/validateToken.js
+++ app/controllers/auth/validateToken.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { getUserIdFromToken } = require("./helpers"),
   {
     isIDGood,
@@ -20,26 +19,3 @@ const validateToken = structure(async (req, res) => {
 });
 
 module.exports = { validateToken };
-=======
-const { getUserIdFromToken } = require("./helpers"),
-  {
-    isIDGood,
-    structure,
-    objSuccess,
-    handleError ,
-  } = require("../../middlewares/utils");
-
-const validateToken = structure(async (req, res) => {
-  const tokenEncrypted = req.headers.authorization
-    .replace("Bearer ", "")
-    .trim();
-  let userId = getUserIdFromToken(tokenEncrypted);
-  userId = isIDGood(userId);
-  if (!userId) return handleError(res, 401, "El token es inválido");
-  res.status(200).json(
-    objSuccess({isValidate: true })
-    );
-});
-
-module.exports = { validateToken };
->>>>>>> 4c1d85377640cc96ff9341775a4418a622f5316a
