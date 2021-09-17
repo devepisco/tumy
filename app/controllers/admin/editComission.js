4c1d85377640cc96ff9341775a4418a622f5316a
meeighteen <josuee.ald@gmail.com>
2021-09-17 14:22:31 -0500

    fix: editComission & ComissionModel

--- app/controllers/admin/editComission.js
+++ app/controllers/admin/editComission.js
@@ -3,8 +3,8 @@ const { structure, objSuccess, handleError } = require("../../middlewares/utils"
 const { Comissions } = require("../../models/NewServices");
 
 const editComission = structure (async(req,res) => {
-    const { data } = matchedData(req);
-    const comission = await Comissions.findOne(data);
+    const data = matchedData(req);
+    const comission = await Comissions.findOne();
     const updatedComission = await Comissions.findByIdAndUpdate(comission._id, data,{new:true});
     if(!updatedComission) return handleError(res, 402,"Error al modificar tarifa de comisión");
     res.status(200).json(objSuccess(updatedComission, "Tarifa de comisión modificada correctamente"));
