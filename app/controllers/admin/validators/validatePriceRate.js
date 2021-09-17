87330d6d5b5609ccf2adef41b2a8af7af46fcbb7
meeighteen <josuee.ald@gmail.com>
2021-09-17 14:57:42 -0500

    fix: ValidatePriceRate

--- app/controllers/admin/validators/validatePriceRate.js
+++ app/controllers/admin/validators/validatePriceRate.js
@@ -3,26 +3,31 @@ const { check } = require('express-validator')
 
 const validatePriceRate = [
     check("IdName")
+        .optional()
         .not()
         .isEmpty()
         .withMessage("Debe ingresar el IdName de la tarifa."),
     check("rateName")
+        .optional()
         .not()
         .isEmpty()
         .withMessage("Debe ingresar el nombre de la tarifa"),
     check("price")
+        .optional()
         .not()
         .isEmpty()
         .withMessage("Debe ingresar el precio correspondiente a la tarifa")
         .isNumeric()
         .withMessage("El precio de la tarifa debe contener caracteres numéricos"),
     check("minPrice")
+        .optional()
         .not()
         .isEmpty()
         .withMessage("Debe ingresar el precio mínimo de la tarifa correspondiente")
         .isNumeric()
         .withMessage("El precio de la tarifa debe contener caracteres numéricos"),
     check("isActive")
+        .optional()
         .not()
         .isEmpty()
         .withMessage("Debe ingresar el estado activo: true/false de la tarifa")
