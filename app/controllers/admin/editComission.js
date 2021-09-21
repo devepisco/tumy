429a251d981a5d32a003e2992f1f23e148fd4f7a
meeighteen <josuee.ald@gmail.com>
2021-09-21 09:22:41 -0500

    fix: commission amount type

--- app/controllers/admin/editComission.js
+++ app/controllers/admin/editComission.js
@@ -5,7 +5,9 @@ const { Comissions } = require("../../models/NewServices");
 const editComission = structure (async(req,res) => {
     const data = matchedData(req);
     const comission = await Comissions.findOne();
-    const updatedComission = await Comissions.findByIdAndUpdate(comission._id, data,{new:true});
+    const updatedComission = await Comissions.findByIdAndUpdate(comission._id, {
+        amount:`${(data.amount*1).toFixed(2)}`
+    },{new:true});
     if(!updatedComission) return handleError(res, 402,"Error al modificar tarifa de comisión");
     res.status(200).json(objSuccess(updatedComission, "Tarifa de comisión modificada correctamente"));
 });
