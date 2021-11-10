74e476788912aa700e89f5b7459539ffd1510d61
meeighteen <josuee.ald@gmail.com>
2021-11-10 06:49:16 -0500

    fix: populate detailState and Global State

--- config/helpers/checkCurrentOrders.js
+++ config/helpers/checkCurrentOrders.js
@@ -8,8 +8,12 @@ const checkCurrentOrders = async (id) => {
   let data = await RequestService.findOne({
     "detail.driverUser": driverId,
     globalState: GlobalStateId,
-  });
-  if (data === null) data = {};
+  })
+    .sort({ _id: -1 })
+    .populate("detailState._id", { _id: 0, IdName: 0, __v: 0 })
+    .populate("globalState", { _id: 0, IdName: 0, __v: 0 })
+    .populate("detail.driverUser", { firstname: 1, lastname: 1 })
+    .exec();
   return data;
 };
 module.exports = { checkCurrentOrders };
