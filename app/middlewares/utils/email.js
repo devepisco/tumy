c65ff3ccb4131f3f2a7beb11d2ba4f920afda54c
meeighteen <josuee.ald@gmail.com>
2021-12-01 06:47:30 -0500

    fix: email.js

--- app/middlewares/utils/email.js
+++ app/middlewares/utils/email.js
@@ -1,27 +1,36 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-const sendEmail = async options => {
-    //create a transporter
+const sendEmail = async (options) => {
+  //create a transporter
+  try {
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: true,
-        service: "gmail",
-        auth: {
-            user:process.env.EMAIL_USER,
-            pass:process.env.EMAIL_PASSWORD
-        }
-    })
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
     //define the email options
     const mailOptions = {
-    from: `"${process.env.EMAIL_NAME}" <${process.env.EMAIL_USER}>`,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    //html: 
+      from: `"${process.env.EMAIL_NAME}" <${process.env.EMAIL_USER}>`,
+      to: options.email,
+      subject: options.subject,
+      text: options.message,
+      //html:
     };
     //actually send the email
     await transporter.sendMail(mailOptions);
-}
+  } catch (err) {
+    return next(
+      new Exceptions(
+        500,
+        "ERROR with transporter."
+      )
+    );
+  }
+};
 
-module.exports= { sendEmail }
+module.exports = { sendEmail };
